398fb0b170892e56c5ca268d4643840ccc669043
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-06-09 15:02:42 -0400

    Card implementation

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,8 @@ import {Helmet} from 'react-helmet';
 import { Container, Row, Col } from 'reactstrap';
 import Header from '.././components/Header';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import './LandingPage.scss';
 
 const LandingPage = () => {
@@ -14,11 +16,15 @@ const LandingPage = () => {
             <Header />
             <Row>
                 <Col xs={12}>
-                    <h2>Hello!</h2>
-                    <p>Welcome to Chrispotify, a modern JS solution to implicit grant authorization.</p>
-                    <Button color='primary' variant='contained' disableElevation>
-                        Authorize Spotify
-                    </Button>
+                    <Card variant='outlined'>
+                        <CardContent>
+                            <h2>Hello!</h2>
+                            <p>Welcome to Chrispotify, a modern JS solution to implicit grant authorization.</p>
+                            <Button color='primary' variant='contained' disableElevation fullWidth>
+                                Authorize Spotify
+                            </Button>
+                        </CardContent>
+                    </Card>
                 </Col>
             </Row>
         </Container>
